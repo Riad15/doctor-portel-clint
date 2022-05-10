faf4968be6b1d71188beaa9e1a625d44ee241e32
asif riad <asifriad15@gmail.com>
2022-05-11 02:39:07 +0600

    add to home banner and information

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Bannar from './Bannar';
+import Info from './Info';
 
 const Home = () => {
     return (
         <div>
-            <h1>this is home page</h1>
+            <Bannar></Bannar>
+            <Info></Info>
         </div>
     );
 };
